410dd0dbdc502037592a3e2588bba1637c1f70cc
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-09-20 21:23:19 +0300

    Empty project created

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YPSamples.SimpleOCR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
